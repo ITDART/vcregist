2c4f48bb2f25cb33e3009073db8d04cbcc921eb9
Takuya Oikawa <takoratta@gmail.com>
2016-04-28 11:46:41 +0900

    Initial MIgration

---
+++ launcher/app/launcher.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace app
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.selectDate = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fstextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.selectFile = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.selectFile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectDate
+            // 
+            this.selectDate.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.selectDate.Location = new System.Drawing.Point(139, 7);
+            this.selectDate.Name = "selectDate";
+            this.selectDate.Size = new System.Drawing.Size(216, 39);
+            this.selectDate.TabIndex = 0;
+            this.selectDate.Value = new System.DateTime(2016, 4, 27, 10, 6, 41, 0);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(396, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "登録";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fstextBox
+            // 
+            this.fstextBox.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.fstextBox.Location = new System.Drawing.Point(139, 55);
+            this.fstextBox.Name = "fstextBox";
+            this.fstextBox.Size = new System.Drawing.Size(353, 39);
+            this.fstextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(60, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "受付日";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(0, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "入力ファイル";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(0, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "出力ファイル";
+            // 
+            // selectFile
+            // 
+            this.selectFile.Image = ((System.Drawing.Image)(resources.GetObject("selectFile.Image")));
+            this.selectFile.Location = new System.Drawing.Point(498, 59);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(27, 30);
+            this.selectFile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.selectFile.TabIndex = 7;
+            this.selectFile.TabStop = false;
+            this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(139, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 39);
+            this.textBox1.TabIndex = 8;
+            // 
+            // MainWindow
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(542, 193);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fstextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.selectDate);
+            this.Name = "MainWindow";
+            this.Text = "災害ボランティア受付";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.MainWindow_DragOver);
+            ((System.ComponentModel.ISupportInitialize)(this.selectFile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker selectDate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fstextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox selectFile;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
